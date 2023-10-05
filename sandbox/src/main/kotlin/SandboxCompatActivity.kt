63124b75186db3bf5622a92e142e986add45ba63
Mark Raynsford <code@io7m.com>
2023-10-05 12:35:55 +0000

    Add support for the legacy toolbar component
    Affects: https://ebce-lyrasis.atlassian.net/browse/PP-389

---
+++ sandbox/src/main/kotlin/SandboxCompatActivity.kt
@@ -0,0 +1,69 @@
+package org.thepalaceproject.theme.sandbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class SandboxCompatActivity : AppCompatActivity(R.layout.sandbox_compat) {
+
+  private lateinit var bottomNavigationView: BottomNavigationView
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    val buttonsOutlinedFragment =
+      ButtonsOutlinedFragment()
+    val buttonsContainedFragment =
+      ButtonsContainedFragment()
+    val buttonsTextFragment =
+      ButtonsTextFragment()
+    val progressFragment =
+      ProgressFragment()
+    val otherFragment =
+      OtherFragment()
+
+    if (savedInstanceState == null) {
+      val fragmentTransaction: FragmentTransaction = this.supportFragmentManager.beginTransaction()
+      fragmentTransaction.replace(R.id.sandboxFragmentContainer, buttonsContainedFragment).commit()
+    }
+
+    this.bottomNavigationView =
+      this.findViewById(R.id.sandboxBottomNavigation)
+
+    this.bottomNavigationView.setOnItemSelectedListener { item ->
+      val fragmentManager: FragmentManager = this.supportFragmentManager
+      return@setOnItemSelectedListener when (item.itemId) {
+        R.id.navButtonsOutlined -> {
+          val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+          fragmentTransaction.replace(R.id.sandboxFragmentContainer, buttonsOutlinedFragment).commit()
+          true
+        }
+        R.id.navButtonsContained -> {
+          val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+          fragmentTransaction.replace(R.id.sandboxFragmentContainer, buttonsContainedFragment).commit()
+          true
+        }
+        R.id.navButtonsText -> {
+          val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+          fragmentTransaction.replace(R.id.sandboxFragmentContainer, buttonsTextFragment).commit()
+          true
+        }
+        R.id.navProgress -> {
+          val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+          fragmentTransaction.replace(R.id.sandboxFragmentContainer, progressFragment).commit()
+          true
+        }
+        R.id.navOther -> {
+          val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+          fragmentTransaction.replace(R.id.sandboxFragmentContainer, otherFragment).commit()
+          true
+        }
+        else -> {
+          true
+        }
+      }
+    }
+  }
+}
