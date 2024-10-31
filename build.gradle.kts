9db2f3751c81db6780d62d072cd69a58e1397a81
Mark Raynsford <code@io7m.com>
2024-10-31 11:45:43 +0000

    Update Android plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,11 +40,11 @@ plugins {
         .apply(false)
 
     id("com.android.library")
-        .version("8.1.0")
+        .version("8.5.0")
         .apply(false)
 
     id("com.android.application")
-        .version("8.1.0")
+        .version("8.5.0")
         .apply(false)
 
     /*
